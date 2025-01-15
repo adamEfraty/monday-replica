3b98e9926ca92a8c62f9c5a9e9a38d8509ad8d18
ReactRay <raydiaz1899@gmail.com>
2025-01-15 22:02:40 +0200

    style updates

--- src/cmps/dynamicCmps/modals/PriorityModal.jsx
+++ src/cmps/dynamicCmps/modals/PriorityModal.jsx
@@ -1,25 +1,26 @@
 
 
-export function PriorityModal({onPriorityChange}){
+export function PriorityModal({ onPriorityChange }) {
 
     //temporary priorities selection
 
 
-    const priorities = [ {text:'Low', color:'#86B6FB'},{text:'Medium', color:'#5559DF'},
-        {text:'High', color:'#401694'},{text:'Critical ⚠️', color:'#333333'},{text:' ', color:'#C4C4C4'}]
+    const priorities = [{ text: 'Low', color: '#86B6FB' }, { text: 'Medium', color: '#5559DF' },
+    { text: 'High', color: '#401694' }, { text: 'Critical ⚠️', color: '#333333' }, { text: ' ', color: '#C4C4C4' }]
 
 
     return (
         <section className="priority-modal">
             {/* list of quick access prioritise */}
-            <ul>
+            <ul className="item-flex">
                 {
-                    priorities.map((priority,index) =>
-                        <li key={index}
-                            onClick={()=>{
+                    priorities.map((priority, index) =>
+                        <li key={index} className="modal-item"
+                            onClick={() => {
                                 console.log(priority)
-                                onPriorityChange(priority)}}
-                            style={{backgroundColor: priority.color}}>
+                                onPriorityChange(priority)
+                            }}
+                            style={{ backgroundColor: priority.color }}>
                             <p>{priority.text}</p>
                         </li>
                     )
--- src/cmps/dynamicCmps/modals/StatusModal.jsx
+++ src/cmps/dynamicCmps/modals/StatusModal.jsx
@@ -1,21 +1,21 @@
 
 
-export function StatusModal({onStatusChange}){
+export function StatusModal({ onStatusChange }) {
 
     //temporary statuses selection
-    const statuses = [{text:'Done', color:'#00C875'},{text:'Working on it', color:'#FDAB3D'},
-        {text:'Stuck', color:'#DF2F4A'},{text:'', color:'#C4C4C4'}]
+    const statuses = [{ text: 'Done', color: '#00C875' }, { text: 'Working on it', color: '#FDAB3D' },
+    { text: 'Stuck', color: '#DF2F4A' }, { text: '', color: '#C4C4C4' }]
 
 
     return (
         <section className="status-modal">
             {/* list of quick access statuses */}
-            <ul>
+            <ul >
                 {
                     statuses.map(status =>
                         <li key={status.text}
-                            onClick={()=>onStatusChange(status)}
-                            style={{backgroundColor: status.color}}>
+                            onClick={() => onStatusChange(status)}
+                            style={{ backgroundColor: status.color }}>
                             <p>{status.text}</p>
                         </li>
                     )
