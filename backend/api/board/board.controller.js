d2c84d01febae2877969211c84b690bf4f844de1
seangah8 <seangah8@gmail.com>
2025-03-04 18:27:54 +0200

    sticky complit

--- backend/api/board/board.controller.js
+++ backend/api/board/board.controller.js
@@ -1,10 +1,8 @@
 import { loggerService } from "../../services/logger.service.js"
 import { boardService } from "./board.service.js"
-import { makeIdForLabel } from "../../services/util.service.js"
 
 
 export async function getBoards(req, res) {
-
     try {
         const boards = await boardService.query()
         res.send(boards)
@@ -27,43 +25,8 @@ export async function getBoard(req, res) {
 }
 
 export async function addBoard(req, res) {
-    const { boardName } = req.body
-    const boardToSave = { 
-        title: boardName, 
-        groups: [],
-        labels: [
-            {
-              id: makeIdForLabel(),
-              type: "taskTitle",
-              name: "task",
-              width: 400,
-            },
-            {
-              id: makeIdForLabel(),
-              type: "priority",
-              name: "priority",
-              width: 150,
-            },
-            {
-              id: makeIdForLabel(),
-              type: "status",
-              name: "status",
-              width: 150,
-            },
-            {
-              id: makeIdForLabel(),
-              type: "members",
-              name: "members",
-              width: 150,
-            },
-            {
-              id: makeIdForLabel(),
-              type: "date",
-              name: "date",
-              width: 150,
-            },
-          ]
-        }
+    const { title, labels, groups } = req.body
+    const boardToSave = { title, labels, groups }
 
     try {
         const savedBoard = await boardService.add(boardToSave)
