28409db83653eeae5f8230495a70c18f581eb9df
seangah8 <seangah8@gmail.com>
2025-03-25 12:54:46 +0200

    fix errors + logo + picture animation

--- frontend/src/cmps/ImageUploader.jsx
+++ frontend/src/cmps/ImageUploader.jsx
@@ -8,26 +8,43 @@ export function ImgUploader({ onUploaded = null }) {
     width: 500,
   });
   const [isUploading, setIsUploading] = useState(false)
+  const [isMoving, setIsMoving] = useState(false)
   const emptyImg = 'https://t4.ftcdn.net/jpg/05/65/22/41/360_F_565224180_QNRiRQkf9Fw0dKRoZGwUknmmfk51SuSS.jpg'
-  
+
   useEffect(()=>{
     if(!imgData.imgUrl)
       setImgData(prev=>({...prev, imgUrl: emptyImg})) 
+      triggerPictureAnimation()
   },[imgData])
 
 
   async function uploadImg(ev) {
-    setIsUploading(true);
-    const { secure_url, height, width } = await uploadService.uploadImg(ev);
-    setImgData({ imgUrl: secure_url, width, height });
-    setIsUploading(false);
-    onUploaded?.(secure_url);
+    setIsMoving(true)
+  
+    setTimeout(async () => {
+      setIsUploading(true)
+      const { secure_url, height, width } = await uploadService.uploadImg(ev)
+      setImgData({ imgUrl: secure_url, width, height })
+      setIsUploading(false)
+  
+      setTimeout(() => {
+        setIsMoving(false)
+      }, 250)
+    }, 250)
+  }
+
+  function triggerPictureAnimation() {
+    setIsMoving(true)
+    setTimeout(() => {
+      setIsMoving(false)
+    }, 1000)
   }
 
   return (
     <section className="image-uploader">
       {imgData.imgUrl && (
-        <img className='profile-picture' src={imgData.imgUrl}/>
+        <img className={`profile-picture ${isMoving ? "move" : ""}`} 
+        src={imgData.imgUrl}/>
       )}
       <input
         type="file"
